e6c383cc63dc6e472c9bddc9366183f3dff6bf53
jtellis <jessetellis@gmail.com>
2016-11-29 16:31:12 -0500

    Check user sequence on piece click

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
 window.onload = function() {
   'use strict';
 
-  let sequence = []; //elements have a value of 1-4 representing pieces
+  /*
+  The sequences elements have a value of 1-4 representing pieces
+  */
+  //Game generated
+  let simonSequence = [];
+  //User generated
+  let userSequence = [];
 
   let randomPiece = function() {
-    Math.floor((Math.random() * 4) + 1);
+    Math.floor((Math.random() * 4) + 1); //Generate rand num 1-4
   };
 
-  let incrementSequence = function() {
+  let incrementSimonSequence = function() {
     sequence.push(randomPiece());
   };
 
-  let resetSequence = function() {
+  let resetSimonSequence = function() {
     squence = [];
   };
 
+  let displaySimonSequence = function() {
+  };
+
+  let correctUserSequence = function() {
+    let currentMove = userSequence.length - 1;
+    if(userSequence[currentMove] === simonSequence[currentMove]) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   let pieceClickHandler = function(event) {
-    console.log(this.dataset.piece);
+    let thisPiece = this.dataset.piece;
+    userSequence.push(thisPiece);
+    if(correctUserSequence) {
+      incrementSimonSequence();
+      displaySimonSequence();
+    }
   };
 
   let pieces = document.getElementsByClassName('piece');
